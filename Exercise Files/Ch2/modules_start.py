bc79e80dd9bd4ba9e75bfaafd8f4abf6a6a92a79
JHeaton97 <87669250+JHeaton97@users.noreply.github.com>
2021-07-19 14:38:15 -0400

    Add files via upload

---
+++ Exercise Files/Ch2/modules_start.py
@@ -0,0 +1,15 @@
+# import the math module, which contains features for working with mathematics
+import math
+
+# the math module contains lots of pre-built functions
+print("The square root of 16 is", math.sqrt(16))
+
+# in addition to functions, some modules contain useful constants
+print("Pi is", math.pi)
+
+# try some of the math functions for yourself here:
+print("Math functions: cos of .7567 is", math.cos(.7567))
+print("Math functions: sin of .7567 is", math.sin(.7567))
+print("Math functions: asin of .7567 is", math.asin(.7567))
+print("Math functions: acos of .7567 is", math.acos(.7567))
+print("Math functions: degrees of 75 is", math.degrees(75))
